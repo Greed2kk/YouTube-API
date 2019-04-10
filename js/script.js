932e3f5451fb3af8a710a10453afdc8578c5bda9
Andrey <draiver78@mail.ru>
2019-04-10 20:18:00 +0300

    minor fix

--- js/script.js
+++ js/script.js
@@ -83,41 +83,78 @@ function dayModeSwitcher() {  // налепил говна
 
     }    
 }
-const data = [
-    ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
-    ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
-        '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки  Урок 2',
-        '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
-    ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
-    ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
-  ];
+
+// const data = [
+//     ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
+//     ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
+//         '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки  Урок 2',
+//         '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
+//     ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
+//     ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
+//   ];
   
-  more.addEventListener('click', () => {
-    const videosWrapper = document.querySelector('.videos__wrapper');
-    more.remove();
+  // more.addEventListener('click', () => {
+  //   const videosWrapper = document.querySelector('.videos__wrapper');
+  //   more.remove();
   
-    for(let i = 0; i < data[0].length; i++) {
-      let card = document.createElement('a');
-      card.classList.add('videos__item', 'videos__item-active');
-      card.setAttribute('data-url', data[3][i]);
-      card.innerHTML = `
-        <img src="${data[0][i]}" alt="thumb">
-        <div class="videos__item-descr">
-          ${data[1][i]}
-        </div>
-        <div class="videos__item-views">
-          ${data[2][i]}
-        </div>
-      `;
-      videosWrapper.appendChild(card);
-      setTimeout(() => {
-        card.classList.remove('videos__item-active');
-      }, 10);
-      bindNewModal(card);
-    }
-    sliceTitle('.videos__item-descr', 80);
-  });
+  //   for(let i = 0; i < data[0].length; i++) {
+  //     let card = document.createElement('a');
+  //     card.classList.add('videos__item', 'videos__item-active');
+  //     card.setAttribute('data-url', data[3][i]);
+  //     card.innerHTML = `
+  //       <img src="${data[0][i]}" alt="thumb">
+  //       <div class="videos__item-descr">
+  //         ${data[1][i]}
+  //       </div>
+  //       <div class="videos__item-views">
+  //         ${data[2][i]}
+  //       </div>
+  //     `;
+  //     videosWrapper.appendChild(card);
+  //     setTimeout(() => {
+  //       card.classList.remove('videos__item-active');
+  //     }, 10);
+  //     if(night === true){
+  //       /*
+  //       card.querySelector('.videos__item-descr').style.color('#fff');
+  //       card.querySelector('.videos__item-views').style.color('#fff');
+  //     }
+  //     */
+  //     bindNewModal(card);
+  //   }
+  //   sliceTitle('.videos__item-descr', 80);
+  // });
   
+function start() {
+    gapi.client.init({
+        'apiKey': 'AIzaSyDoLxZfaNGzRvhm47TSEQ5lIH0zw-fMfIo',
+        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
+    }).then(function() {
+        return gapi.client.youtube.playlistItems.list({
+          "part": "snippet,contentDetails",
+          "maxResults": '6', // убрать ковычки
+          "playlistId": "PLI_7Mg2Z_-4Ke14LWWl5z42dhA0F5GNpS"
+        })    
+    }).then(function(response){
+          console.log(response.result);
+    }).catch( e => {
+          console.log(e);
+    });
+}
+
+more.addEventListener('click', () => {
+  more.remove();
+  gapi.load('client', start);
+});
+
+
+
+
+
+
+
+
+
   function sliceTitle(selector, count) {
     document.querySelectorAll(selector).forEach(item => {
       item.textContent.trim();
@@ -201,6 +238,12 @@ const data = [
 
 
 
+
+
+
+//PL3LQJkGQtzc4gsrFkm4MjWhTXhopsMgpv
+
+// AIzaSyDoLxZfaNGzRvhm47TSEQ5lIH0zw-fMfIo
 
 /*
 const data = [
